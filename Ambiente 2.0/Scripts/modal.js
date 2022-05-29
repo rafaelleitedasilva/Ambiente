6a668ff9eab830eaa4357050ec8fff930a29388e
Rafael leite <69431917+rafaelleitedasilva@users.noreply.github.com>
2022-05-29 16:54:46 -0300

    29/05/2022

--- Ambiente 2.0/Scripts/modal.js
+++ Ambiente 2.0/Scripts/modal.js
@@ -7,6 +7,9 @@ let divModal = document.createElement("div");
 let divRegistros = document.createElement("div");
 let tabela = document.createElement("table");
 let divForm = document.createElement("div");
+let labelProfessor = document.createElement("label")
+let labelData = document.createElement("label")
+let labelHora = document.createElement("label")
 let buttonX = document.createElement("button")
 
 //Elementos do Cabeçalho da Tabela
@@ -27,25 +30,17 @@ buttonX.className="btn btn-danger-modal"
 thProfessor.scope,thData.scope,thHora.scope,thDelete.scope = "col";
 divForm.className="form"
 
-//Criando o Modal
-divContainer.appendChild(divModal)
-divModal.appendChild(divRegistros)
-divRegistros.appendChild(tabela)
-tabela.appendChild(thead)
-thead.appendChild(trThead)
-trThead.appendChild(thProfessor)
-trThead.appendChild(thData)
-trThead.appendChild(thHora)
-trThead.appendChild(thDelete)
-divModal.appendChild(divForm)
-divModal.appendChild(buttonX)
-
 //Valores
 thProfessor.innerText="Professor";
 thData.innerText="Data"
 thHora.innerText="Horário"
 thDelete.innerText="Apagar"
 buttonX.innerText="X"
+labelProfessor.innerText="Professor:"
+labelData.innerText="Data:"
+labelHora.innerText="Horário"
+
+
 
 //Função do ButtonX
 buttonX.onclick = function(){
@@ -55,11 +50,47 @@ buttonX.onclick = function(){
 
 function cria(id){
     divContainer.style="display:flex;"
-    let tbody = document.createElement("tbody")
-    tbody.id = `tbody${id}`;
 
+    let tbody = document.createElement("tbody")
+    let br = document.createElement("br")
+    let inputProfessor = document.createElement("input")
+    let inputData = document.createElement("input")
+    let submit = document.createElement("input")
+    let inputHora = document.createElement("input")
+
+    //Inputs
+    inputProfessor.type="text"
+    inputData.type="date"
+    inputHora.type="time"
+    submit.type="submit"
+    submit.value="Confirmar"
+    submit.className="btn btn-success"
+    tbody.id = `tbody${id}`;
+    inputProfessor.id=`professor${id}`
+    inputData.id=`data${id}`
+    inputHora.id=`hora${id}`
+    
     body.appendChild(divContainer);
-    tabela.appendChild(tbody);
+    divContainer.appendChild(divModal)
+    divModal.appendChild(divRegistros)
+    divRegistros.appendChild(tabela)
+    tabela.appendChild(thead)
+    thead.appendChild(trThead)
+    trThead.appendChild(thProfessor)
+    trThead.appendChild(thData)
+    trThead.appendChild(thHora)
+    trThead.appendChild(thDelete)
+    divModal.appendChild(divForm)
+    divModal.appendChild(buttonX)
+    tabela.appendChild(tbody)
+    divForm.appendChild(labelProfessor)
+    divForm.appendChild(inputProfessor)
+    divForm.appendChild(labelData)
+    divForm.appendChild(inputData)
+    divForm.appendChild(br)
+    divForm.appendChild(labelHora)
+    divForm.appendChild(inputHora)
+    divForm.appendChild(submit)
     
 
 }
