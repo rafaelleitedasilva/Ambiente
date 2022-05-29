11d151e42a38b4a7af2449d8e24c780bc4ab37ac
Rafael leite <69431917+rafaelleitedasilva@users.noreply.github.com>
2022-05-29 18:00:25 -0300

    Issue: Tbody values shared

--- Ambiente 2.0/Scripts/modal.js
+++ Ambiente 2.0/Scripts/modal.js
@@ -1,5 +1,7 @@
 //Setando o Body em uma variável
 let body = document.getElementById("body");
+let check = [];
+let x = 0;
 
 //Criando os Elementos do Modal
 let divContainer = document.createElement("div");
@@ -47,16 +49,18 @@ buttonX.onclick = function(){
     divContainer.style="display:none;"
 }
 
-
-function cria(id){
-    divContainer.style="display:flex;"
-
+    //Elementos Específicos de Cada Sala
     let tbody = document.createElement("tbody")
     let br = document.createElement("br")
     let inputProfessor = document.createElement("input")
     let inputData = document.createElement("input")
     let submit = document.createElement("input")
     let inputHora = document.createElement("input")
+
+    
+
+function cria(id){
+    divContainer.style="display:flex;"
 
     //Inputs
     inputProfessor.type="text"
@@ -66,10 +70,12 @@ function cria(id){
     submit.value="Confirmar"
     submit.className="btn btn-success"
     tbody.id = `tbody${id}`;
+    console.log(tbody.id)
     inputProfessor.id=`professor${id}`
     inputData.id=`data${id}`
     inputHora.id=`hora${id}`
     
+    //Criando o Model proceduralmente
     body.appendChild(divContainer);
     divContainer.appendChild(divModal)
     divModal.appendChild(divRegistros)
@@ -91,6 +97,74 @@ function cria(id){
     divForm.appendChild(labelHora)
     divForm.appendChild(inputHora)
     divForm.appendChild(submit)
-    
+
+    submit.setAttribute("onclick", `confirmaSala(${id})`)
 
 }
+
+
+
+function confirmaSala(id){
+    //Pegando os valores do input e colocando em variáveis
+    let professor = document.getElementById(`professor${id}`)
+    let data = document.getElementById(`data${id}`)
+    let hora = document.getElementById(`hora${id}`) 
+
+    let tr = document.createElement('tr')
+    let cancela = document.createElement('button')
+    cancela.className = "btn-danger"
+    let tbody = document.getElementById(`tbody${id}`);  
+    let tdOne = document.createElement('td')
+    tdOne.appendChild(cancela)
+    let tdTwo = document.createElement('td')
+    tdTwo.innerHTML = String(professor.value);
+    let tdThree = document.createElement('td')
+    let tdFour = document.createElement('td')
+
+    //Variável que vai garantir a atualização dos dados
+    let y = 1;
+
+    //Checando se o horário e data escolhidos já foram selecionados por outro professor
+    for(let i = 0; i<check.length; i++){
+        if(check[i] == `${data.value}${hora.value}`){
+            window.alert("Essa data e hora já foram escolhidas por outro professor!");
+            y = 0;
+        }
+    }
+
+    //Se o valor de y permanecer sendo 1, significa que seus valores não foram encontrados na array, ou seja, os valores podem ser adicionados
+
+    if(y == 1){
+        //Adicionando o texto da data e hora.
+            tdThree.innerHTML = String(data.value);
+            tdFour.innerHTML = String(hora.value);
+        //Colocando o valor da data e hora na array check2
+            check.push(`${data.value}${hora.value}`);
+            console.log(check)
+        //Adicionando tudo visualmente para o usuário
+            tbody.appendChild(tr)
+            tr.appendChild(tdTwo)
+            tr.appendChild(tdThree)
+            tr.appendChild(tdFour)
+            tr.appendChild(tdOne)
+    }
+
+    cancela.innerText = "X"
+
+    //Atrelando uma função no botão Cancela para que ele apague o elemento visualmente e na array
+    cancela.onclick = function(){
+
+        //Pega a tr mais próxima do botão de apagar
+        let linha = cancela.closest("tr");
+
+        //Cria um for que checa todos os elementos da array e quando encontra o valor que deseja apaga ele, junto com a linha
+        for (let i = 0; i < check.length; i++){
+            if(check[i] == `${linha.childNodes[1].textContent}${linha.childNodes[2].textContent}`){
+                check.splice(i,1)
+                cancela.closest("tr").remove();
+                console.log(check)
+            }
+        }
+        
+    }  
+    }
